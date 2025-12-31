9c0fa71bf85caf3c80c746a6f7d2774bfb14924e
ace347 <acesigdel@gmail.com>
2025-12-31 19:15:47 +0545

    Cart item /// dummy items done

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let shop=document.getElementById("btn-shopnow");
 let container=document.getElementById("hf-continer");
 let buySell =document.getElementById("hiddenclass");
+let attcbtn=document.getElementById("attc-btn");
 buySell.classList.add("hiddenclass");
 shop.addEventListener("click",function(){
   container.classList.add("hiddenclass");
   buySell.classList.remove("hiddenclass");
+
 });
+ attcbtn.addEventListener("click",function(){
+  buySell.classList.add("hiddenclass");
+  container.classList.remove("hiddenclass");  });
