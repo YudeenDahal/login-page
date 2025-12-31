c5e9a8f8cd6894320354060f81cca71773324be9
ace347 <acesigdel@gmail.com>
2025-12-31 10:22:29 +0545

    Basic structure

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let shop=document.getElementById("btn-shopnow");
 let container=document.getElementById("hf-continer");
+let buySell =document.getElementById("hiddenclass");
+buySell.classList.add("hiddenclass");
 shop.addEventListener("click",function(){
   container.classList.add("hiddenclass");
+  buySell.classList.remove("hiddenclass");
 });
